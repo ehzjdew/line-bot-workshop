d4b754d693bbef88be092349907f68d890efdbef
ehzjdew <46664153+ehzjdew@users.noreply.github.com>
2019-01-14 11:49:23 +0700

    Update verify.php

--- Desktop/line-bot-unlimit_friend-master/verify.php
+++ Desktop/line-bot-unlimit_friend-master/verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '8abc5b0249afebf2f293f9181634ec83';
+$access_token = 'b6BN76vbMHuX52JURAZ9a0il0yIOyiZOy3BlJ1YTYZr82i2kta0IFENvZs015u7tAFjEv70IUxptIhe+W0cAT/VxMx1ZHF2xQfwtiQyhN1chucmpzcfb7j+WNfEHZ6W4cHw7qXko0p1AAgeeG6iPFQdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
